65f96cfa4e89a5c3d08c37f97dbb66f29663fc16
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-04-29 19:13:51 -0700

    Made changes to reflect new structure of classes

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,26 @@
 int main(int argc, char* argv[])
 {
     // Create Game object
-    Game* game = new Game();
-
-    // initialize SDL
-    game->init("My First SDL Game", 640, 480);
-
-    //Succeeded; Begin game loop
-    while (game->running()) {
-        game->handleEvents();
-        game->update();
-        game->render();
-        //Add delay
-        SDL_Delay(10);
+    std::cout << "Game initializing...\n";
+    if (TheGame::Instance()->init("My First SDL Game", 640, 480)) {
+        //Succeeded; Begin game loop
+        while (TheGame::Instance()->running()) {
+            TheGame::Instance()->handleEvents();
+            TheGame::Instance()->update();
+            TheGame::Instance()->render();
+            //Add delay
+            SDL_Delay(10);
+        }
+    }
+    else {
+        std::cout << "Game failed to initialize.\n";
+        return -1;
     }
 
+    std::cout << "Game initialized successfully\n";
+
     //Exit game
-    delete game;
+    delete TheGame::Instance();
 
     return 0;
 }
