50d805f5b5b26510cb59358ecbabe161b1277b53
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-05-03 04:23:53 -0700

    Set 60 FPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include "Game.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main(int argc, char* argv[])
 {
+    Uint32 frameStart, frameTime;
+
     // Create Game object
     std::cout << "Game initializing...\n";
     if (TheGame::Instance()->init("My First SDL Game", 640, 480)) {
         //Succeeded; Begin game loop
         while (TheGame::Instance()->running()) {
+            //Get start frame time
+            frameStart = SDL_GetTicks64();
+
             TheGame::Instance()->handleEvents();
             TheGame::Instance()->update();
             TheGame::Instance()->render();
-            //Add delay
-            SDL_Delay(10);
+
+            //Get end frame time
+            frameTime = SDL_GetTicks64() - frameStart;
+
+            //Add delay if necessary
+            if (frameTime < DELAY_TIME) {
+                SDL_Delay((int)(DELAY_TIME - frameTime));
+            }
         }
     }
     else {
