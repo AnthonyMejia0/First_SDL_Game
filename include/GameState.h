ef3320a34172ac54416cc8725bf3cb399fd212e4
AnthonyMejia0 <mejia.anthony96@gmail.com>
2022-05-20 23:06:26 -0700

    Added state machine

---
+++ include/GameState.h
@@ -0,0 +1,17 @@
+#ifndef __GAMESTATE_H__
+#define __GAMESTATE_H__
+
+#include <iostream>
+#include <string>
+
+class GameState {
+public:
+    virtual ~GameState() {}
+    virtual void update() = 0;
+    virtual void render() = 0;
+    virtual bool onEnter() = 0;
+    virtual bool onExit() = 0;
+    virtual std::string getStateID() const = 0;
+};
+
+#endif //__GAMESTATE_H__
